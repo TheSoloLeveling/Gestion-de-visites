d2a7b764a2a56fe087ea506812fbe55ffbce93ec
madfalouh <70645288+madfalouh@users.noreply.github.com>
2021-05-18 16:21:08 +0100

    mm

--- JavaApplication4/src/javaapplication4/MainController.java
+++ JavaApplication4/src/javaapplication4/MainController.java
@@ -9,6 +9,7 @@ package javaapplication4;
 
 
 
+import Home.SplashScreenController;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXToggleButton;
